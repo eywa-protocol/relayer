a85c481cd05489c9f0e6a742fc996eba53815ac7
bogdan sivochkin <syi3@ya.ru>
2021-04-20 10:18:13 +0300

    added ListenOracleRequest and FilterOracleRequestEvent

---
+++ helpers/helper.go
@@ -0,0 +1,75 @@
+package helpers
+
+import (
+	"context"
+	wrappers "github.com/DigiU-Lab/eth-contracts-go-wrappers"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/sirupsen/logrus"
+)
+
+type OracleRequest struct {
+	requestType    string
+	bridge         common.Address
+	requestId      [32]byte
+	selector       []byte
+	receiveSide    common.Address
+	oppositeBridge common.Address
+}
+
+func FilterOracleRequestEvent(client ethclient.Client, start uint64, contractAddress common.Address) (oracleRequest OracleRequest, err error) {
+	mockFilterer, err := wrappers.NewBridge(contractAddress, &client)
+	if err != nil {
+		return
+	}
+
+	it, err := mockFilterer.FilterOracleRequest(&bind.FilterOpts{Start: start, Context: context.Background()})
+	if err != nil {
+		return
+	}
+
+	for it.Next() {
+		logrus.Print("OracleRequest Event", it.Event.Raw)
+		if it.Event != nil {
+			oracleRequest = OracleRequest{
+				requestType:    it.Event.RequestType,
+				bridge:         it.Event.Bridge,
+				requestId:      it.Event.RequestId,
+				selector:       it.Event.Selector,
+				receiveSide:    it.Event.ReceiveSide,
+				oppositeBridge: it.Event.OppositeBridge,
+			}
+		}
+	}
+	return
+}
+
+func ListenOracleRequest(client ethclient.Client) (oracleRequest OracleRequest, err error) {
+	channel := make(chan *wrappers.BridgeOracleRequest)
+	opt := &bind.WatchOpts{}
+	sub, err := wrappers.BridgeFilterer.WatchOracleRequest(opt, opt, channel)
+	if err != nil {
+		return
+	}
+
+	go func() {
+		for {
+			select {
+			case err := <-sub.Err():
+				logrus.Println("OracleRequest error:", err)
+			case event := <-channel:
+				logrus.Printf("OracleRequest id: %d type: %d\n", event.RequestId, event.RequestType)
+				oracleRequest = OracleRequest{
+					requestType:    event.RequestType,
+					bridge:         event.Bridge,
+					requestId:      event.RequestId,
+					selector:       event.Selector,
+					receiveSide:    event.ReceiveSide,
+					oppositeBridge: event.OppositeBridge,
+				}
+			}
+		}
+	}()
+	return
+}

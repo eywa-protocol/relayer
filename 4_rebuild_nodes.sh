3fb8d9b1cc42a85d7497b564903fcfeedf03b72f
bogdan sivochkin <syi3@ya.ru>
2021-05-10 12:32:29 +0300

    moved keys init to loadconfig section

--- 4_rebuild_nodes.sh
+++ 4_rebuild_nodes.sh
@@ -1,7 +1,8 @@
 make clean
 docker-compose up -d --build --no-deps node
-docker-compose up -d --scale node=7
-docker restart $(docker ps -a --format "{{.Names}}" | grep node | sort -n)
+docker-compose up -d --scale node=30
+#docker restart $(docker ps -a --format "{{.Names}}" | grep node | sort -n)
+docker start $(docker ps -f "status=exited" --format "{{.Names}}" | grep node)
 docker-compose logs -f -t | grep -v ganache
 
 

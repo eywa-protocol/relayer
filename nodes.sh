a5aae4e00b0b54f0ea7012028993bd5074c1cba6
bogdan sivochkin <syi3@ya.ru>
2021-05-26 19:41:27 +0300

    removed orphaned

--- nodes.sh
+++
@@ -1,3 +0,0 @@
-docker-compose up -d --build --scale node=7 --force-recreate
-#docker restart $(docker ps -a --format "{{.Names}}") # | grep node | sort -n)
-docker-compose logs -f -t | grep -v ganache

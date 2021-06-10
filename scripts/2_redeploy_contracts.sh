179e012482f63e36c137a6bcdc92cab172f278bc
bogdan sivochkin <syi3@ya.ru>
2021-06-10 15:42:04 +0300

    rebuild ganache

--- scripts/2_redeploy_contracts.sh
+++ scripts/2_redeploy_contracts.sh
@@ -7,15 +7,15 @@ if [[ "$OSTYPE" == "darwin"* ]];then
   fi
   echo "compose for macos docker host"
   docker-compose -f ../docker-compose-macos.yaml stop && \
-  docker-compose -f ../docker-compose-macos.yaml up -d ganache_net1 && \
-  docker-compose -f ../docker-compose-macos.yaml up -d ganache_net2 && \
+  docker-compose -f ../docker-compose-macos.yaml up -d --build ganache_net1 && \
+  docker-compose -f ../docker-compose-macos.yaml up -d --build ganache_net2 && \
   make -C ../external/eth-contracts && \
   make -C ../external/eth-contracts eth-local-migrate
 else
   echo "compose for linux docker host"
   docker-compose stop && \
-  docker-compose up -d ganache_net1 && \
-  docker-compose up -d ganache_net2 && \
+  docker-compose up -d --build ganache_net1 && \
+  docker-compose up -d --build ganache_net2 && \
   make -C ../external/eth-contracts && \
   make -C ../external/eth-contracts eth-local-migrate
 fi

24e966d1d20818f59082aea760a701488dba357f
Bogdan Sivochkin <b.sivochkin@digiu.ai>
2021-07-12 01:02:17 +0200

    tests

--- test/testnets_test.go
+++ test/testnets_test.go
@@ -35,32 +35,32 @@ func init() {
 }
 
 func Test_SendRequestV2_FromRinkebyToMumbai(t *testing.T) {
-	SendRequestV2FromChainToChain(t, big.NewInt(1111), big.NewInt(1112), big.NewInt(rand.Int63()))
+	SendRequestV2FromChainToChain(t, big.NewInt(4), big.NewInt(80001), big.NewInt(rand.Int63()))
 	require.NoError(t, err)
 }
 
 func Test_SendRequestV2_FromMumbaiToRinkeby(t *testing.T) {
-	SendRequestV2FromChainToChain(t, big.NewInt(1112), big.NewInt(1111), big.NewInt(rand.Int63()))
+	SendRequestV2FromChainToChain(t, big.NewInt(80001), big.NewInt(4), big.NewInt(rand.Int63()))
 	require.NoError(t, err)
 }
 
 func Test_SendRequestV2_FromRinkebyToBsc(t *testing.T) {
-	SendRequestV2FromChainToChain(t, big.NewInt(1111), big.NewInt(1113), big.NewInt(rand.Int63()))
+	SendRequestV2FromChainToChain(t, big.NewInt(4), big.NewInt(97), big.NewInt(rand.Int63()))
 	require.NoError(t, err)
 }
 
 func Test_SendRequestV2_FromBscToRinkeby(t *testing.T) {
-	SendRequestV2FromChainToChain(t, big.NewInt(1113), big.NewInt(1111), big.NewInt(rand.Int63()))
+	SendRequestV2FromChainToChain(t, big.NewInt(97), big.NewInt(4), big.NewInt(rand.Int63()))
 	require.NoError(t, err)
 }
 
 func Test_SendRequestV2_FromBscToMumbai(t *testing.T) {
-	SendRequestV2FromChainToChain(t, big.NewInt(1112), big.NewInt(1113), big.NewInt(rand.Int63()))
+	SendRequestV2FromChainToChain(t, big.NewInt(97), big.NewInt(80001), big.NewInt(rand.Int63()))
 	require.NoError(t, err)
 }
 
 func Test_SendRequestV2_FromMumbaiToBsc(t *testing.T) {
-	SendRequestV2FromChainToChain(t, big.NewInt(1113), big.NewInt(1112), big.NewInt(rand.Int63()))
+	SendRequestV2FromChainToChain(t, big.NewInt(80001), big.NewInt(97), big.NewInt(rand.Int63()))
 	require.NoError(t, err)
 }
 

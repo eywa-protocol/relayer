47674a47f50427390ddf9ce4e474cfc6f83d3ab7
bogdan sivochkin <syi3@ya.ru>
2021-07-12 18:07:20 +0300

    updated submodules path and deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-
-if [[ ! -d ../keys ]];then
-  mkdir ../keys
-fi
-./1_clean_nodes.sh
+./fix_docker_perms.sh && \
+./1_clean_nodes.sh && \
 ./2_redeploy_contracts.sh && \
+./fix_docker_perms.sh && \
 ./3_build_bsn.sh && \
 ./4_build_config.sh && \
 ./5_build_nodes.sh

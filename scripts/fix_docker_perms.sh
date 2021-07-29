091344eb66a7586706db2f2f876f33114ff36cd5
bogdan sivochkin <syi3@ya.ru>
2021-07-29 15:25:44 +0300

    recreate .data on start , updated submodule version

--- scripts/fix_docker_perms.sh
+++ scripts/fix_docker_perms.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+DIRECTORY="../.data"
+
+if [[ -d "$DIRECTORY" ]];then
+	rm -rf $DIRECTORY
+	mkdir -p $DIRECTORY
+fi
+
 if [[ "$OSTYPE" == "linux"* ]];then
   sudo chown -R $USER:$USER ../.data/
 fi

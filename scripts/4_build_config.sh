d8ab3976f8675a4c7ff42fbeb42a7e9a2fec9d4b
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-08-12 12:02:59 +0300

    fix for ubuntu

--- scripts/4_build_config.sh
+++ scripts/4_build_config.sh
@@ -15,7 +15,7 @@ if [[ "$OSTYPE" == "darwin"* ]];then
   export RANDEVOUE=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w ${1:-32} | head -n 1)
   export SED=gsed
 else
-  export RANDEVOUE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1)
+  export RANDEVOUE=$(head -80 /dev/urandom | LC_ALL=c tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
   export SED=sed
 fi
 

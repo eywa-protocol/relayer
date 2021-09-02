8c76c51bcedf0a98b4e37a16743bf377aae4527f
Dmitriy Khristianov <support@dkh.ru>
2021-09-02 13:12:10 +0300

    remove common dependencies from forward package

--- forward/base.go
+++ forward/base.go
@@ -2,6 +2,8 @@ package forward
 
 import (
 	"crypto/ecdsa"
+	"encoding/hex"
+	"errors"
 	"fmt"
 	"math/big"
 	"strings"
@@ -10,8 +12,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
-	signer "github.com/ethereum/go-ethereum/signer/core"
-	common2 "gitlab.digiu.ai/blockchainlaboratory/eywa-p2p-bridge/common"
+	"github.com/ethereum/go-ethereum/signer/core"
 	"gitlab.digiu.ai/blockchainlaboratory/wrappers"
 )
 
@@ -20,13 +21,18 @@ const (
 	forwardRequest = "ForwardRequest"
 )
 
+var (
+	ErrStringTooLong = errors.New("string to long")
+	ErrHexTooShort   = errors.New("hex string is shorter than bytes32")
+)
+
 type GsnCaller interface {
 	GetForwarder(chainId *big.Int) (*wrappers.Forwarder, error)
 	GetForwarderAddress(chainId *big.Int) (common.Address, error)
 	Execute(chainId *big.Int, req wrappers.IForwarderForwardRequest, domainSeparator [32]byte, requestTypeHash [32]byte, suffixData []byte, sig []byte) (common.Hash, error)
 }
 
-func NewForwardRequestTypedData(req *wrappers.IForwarderForwardRequest, forwarderAddress, abiJson, methodName string, args ...interface{}) (*signer.TypedData, error) {
+func NewForwardRequestTypedData(req *wrappers.IForwarderForwardRequest, forwarderAddress, abiJson, methodName string, args ...interface{}) (*core.TypedData, error) {
 
 	contractAbi, err := abi.JSON(strings.NewReader(abiJson))
 	if err != nil {
@@ -40,11 +46,11 @@ func NewForwardRequestTypedData(req *wrappers.IForwarderForwardRequest, forwarde
 
 	req.Data = contractAbiPacked
 
-	return &signer.TypedData{
-		Types: signer.Types{
-			domainType: []signer.Type{{
+	return &core.TypedData{
+		Types: core.Types{
+			domainType: []core.Type{{
 				Name: "verifyingContract", Type: "address"}},
-			forwardRequest: []signer.Type{
+			forwardRequest: []core.Type{
 				{
 					Name: "from", Type: "address"},
 				{
@@ -59,11 +65,11 @@ func NewForwardRequestTypedData(req *wrappers.IForwarderForwardRequest, forwarde
 					Name: "data", Type: "bytes"},
 			},
 		},
-		Domain: signer.TypedDataDomain{
+		Domain: core.TypedDataDomain{
 			VerifyingContract: forwarderAddress,
 		},
 		PrimaryType: "ForwardRequest",
-		Message: signer.TypedDataMessage{
+		Message: core.TypedDataMessage{
 			"from":  req.From.String(),
 			"to":    req.To.String(),
 			"value": req.Value.String(),
@@ -74,40 +80,89 @@ func NewForwardRequestTypedData(req *wrappers.IForwarderForwardRequest, forwarde
 	}, nil
 }
 
-func NewSignature(typedData *signer.TypedData, signer *ecdsa.PrivateKey) (signature hexutil.Bytes, SignDataRequestHash hexutil.Bytes, err error) {
+func NewSignature(typedData *core.TypedData, signer *ecdsa.PrivateKey) (signature hexutil.Bytes, SignDataRequestHash hexutil.Bytes, err error) {
 
 	if addr, err := common.NewMixedcaseAddressFromString(crypto.PubkeyToAddress(signer.PublicKey).String()); err != nil {
 
 		return nil, nil, err
 	} else {
 
-		return common2.SignTypedData(*signer, *addr, *typedData)
+		return SignTypedData(*signer, *addr, *typedData)
 	}
 }
 
-func NewDomainSeparatorHash(typedData *signer.TypedData) (common2.Bytes32, error) {
+func NewDomainSeparatorHash(typedData *core.TypedData) ([32]byte, error) {
 
 	if domainSeparator, err := typedData.HashStruct(domainType, typedData.Domain.Map()); err != nil {
 
-		return common2.Bytes32{}, err
+		return [32]byte{}, err
 	} else {
 
-		return common2.FromHex(domainSeparator.String())
+		return FromHex(domainSeparator.String())
 	}
 
 }
 
-func NewRequestTypeHash(genericParams string) (common2.Bytes32, error) {
+func NewRequestTypeHash(genericParams string) ([32]byte, error) {
 
 	forwardRequestType := fmt.Sprintf("%s(%s)", forwardRequest, genericParams)
 	reqTypeHash := crypto.Keccak256([]byte(forwardRequestType))
 
-	if reqTypeHashBytes32, err := common2.BytesToBytes32(reqTypeHash); err != nil {
+	if reqTypeHashBytes32, err := BytesToBytes32(reqTypeHash); err != nil {
 
-		return common2.Bytes32{}, err
+		return [32]byte{}, err
 	} else {
 
 		return reqTypeHashBytes32, nil
 	}
 
+}
+
+func BytesToBytes32(b []byte) (r [32]byte, err error) {
+	if len(b) > 32 {
+		err = ErrStringTooLong
+	} else {
+		copy(r[:], b)
+	}
+	return
+}
+
+func FromHex(s string) ([32]byte, error) {
+	var b [32]byte
+	n, err := hex.Decode(b[:], []byte(strings.TrimPrefix(s, "0x")))
+	if err == nil && n != 32 {
+		return b, ErrHexTooShort
+	}
+	return b, err
+}
+
+func SignTypedData(key ecdsa.PrivateKey, addr common.MixedcaseAddress, typedData core.TypedData) (signature hexutil.Bytes, SignDataRequestHash hexutil.Bytes, err error) {
+
+	domainSeparator, err := typedData.HashStruct("EIP712Domain", typedData.Domain.Map())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	typedDataHash, err := typedData.HashStruct(typedData.PrimaryType, typedData.Message)
+	if err != nil {
+		return nil, nil, err
+	}
+	rawData := []byte(fmt.Sprintf("\x19\x01%s%s", string(domainSeparator), string(typedDataHash)))
+	sigHash := crypto.Keccak256(rawData)
+	messages, err := typedData.Format()
+	if err != nil {
+		return nil, nil, err
+	}
+	req := &core.SignDataRequest{
+		ContentType: core.DataTyped.Mime,
+		Rawdata:     rawData,
+		Messages:    messages,
+		Hash:        sigHash,
+		Address:     addr}
+	signature, err = crypto.Sign(req.Hash, &key)
+	if err != nil {
+		return nil, nil, err
+	}
+	signature[64] += 27
+	return signature, req.Hash, nil
 }

0b007c6c0baa456557660dfc8f0594d1811d21f5
v0ld <v0ld@ai>
2021-09-30 11:50:13 +0700

    update mock. test before move from main

--- store/main/main.go
+++ store/main/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"math/big"
+	"os"
 	"time"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/sirupsen/logrus"
@@ -25,7 +29,7 @@ func (bc *testBlockChain) SubscribeChainHeadEvent(ch chan<- store.ChainHeadEvent
 
 
 func main()  {
-	logrus.SetLevel(logrus.Level(5))
+	logrus.SetLevel(logrus.Level(4))
 	// create mock blockchain
 	bc := &testBlockChain{}
 	// create tx pool (event pool). Subscribe on event ChainHeadEvent. Started eventLoop.
@@ -33,29 +37,48 @@ func main()  {
 	defer txPool.Stop()
 
 
-	// emulate event OracleRequest
+	// emulate event OracleRequest during 50 sec
 	checkClientTimer := time.NewTicker(10 * time.Second)
 	go func(ch chan <-store.NewEventSourceChain)  {
-		defer checkClientTimer.Stop()
+		ctx, cancel := context.WithTimeout(context.Background(), 50*time.Second)
+		defer func() {
+			checkClientTimer.Stop()
+			cancel()
+			time.Sleep(10 * time.Second)
+			pending, _ := txPool.Pending()
+			logrus.Debugln("Ended...\n", spew.Sdump(pending))
+			logrus.Debugln("================================================================")
+			pendingByChainId, _ := txPool.PendingByChainId()
+			for k, v := range pendingByChainId {
+				for _, vv := range v {
+					logrus.Info(vv.ChainId(), vv.Nonce())
+				}
+			}
+			logrus.Debugln("Ended 2...\n", spew.Sdump(pendingByChainId))
+			os.Exit(0)
+		}()
 		for {
 			select {
+			case <-ctx.Done():
+				return
 			case <-checkClientTimer.C:
 				logrus.Info("Sended mock OracleRequest...")
 				ch <- store.NewEventSourceChain{
 					EventSourceChain: &wrappers.BridgeOracleRequest{
 						RequestType: "setRequest",
 						Bridge: common.HexToAddress("0x376c47978271565f56DEB45495afa69E59c16Ab2"),
+						Chainid: big.NewInt(4),
 					},
 				}
 				ch <- store.NewEventSourceChain{
 					EventSourceChain: &wrappers.BridgeOracleRequest{
 						RequestType: "setRequest",
 						Bridge: common.HexToAddress("0x0c760E9A85d2E957Dd1E189516b6658CfEcD3985"),
+						Chainid: big.NewInt(94),
 					},
 				}
 
 			}
 		}
 	}(txPool.EventSourceReq)
-
 }

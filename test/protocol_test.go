aaf0d1ad7c86f0c229f4ab497471138d83fcd243
Denis Glotov <denis@glotov.org>
2021-11-10 16:51:43 +0100

    Fix custom_bls_test

--- test/protocol_test.go
+++ test/protocol_test.go
@@ -141,6 +141,10 @@ func setupHostsBLS(n int, initialPort int) ([]*bridge.Node, []*core.Host) {
 }
 
 func makeSession(n int, node *bridge.Node) *model.Node {
+	topic, err := node.P2PPubSub.JoinTopic("ProtocolTestTopic")
+	if err != nil {
+		logrus.Error(err)
+	}
 	return &model.Node{
 		Ctx:            node.Ctx,
 		Id:             node.Id,
@@ -158,6 +162,7 @@ func makeSession(n int, node *bridge.Node) *model.Node {
 		MembershipKey:  node.MembershipKey,
 		PartPublicKey:  bls.ZeroPublicKey(),
 		PartSignature:  bls.ZeroSignature(),
+		Topic:          topic,
 	}
 }
 
